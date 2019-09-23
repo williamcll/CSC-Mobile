1a3eb5fa433d0f9e0cffaf84185f7044eba7c1bd
williamcll <williamcll@outlook.com>
2019-09-23 17:04:07 +0800

    Nested  Please enter the commit message for your changes. Lines starting

---
+++ Nested/app/src/main/java/com/chuhai/nested/MainActivity.java
@@ -0,0 +1,29 @@
+package com.chuhai.nested;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*
+     * Called when the various radio buttons are clicked.
+     * Changes which turtle image is showing in the ImageView on screen.
+     */
+    public void radioClick(View view) {
+        //Task 1: Get the imageview object
+
+
+        //Task 2: choose image to show based on ID of radio button clicked
+    }
+
+}
