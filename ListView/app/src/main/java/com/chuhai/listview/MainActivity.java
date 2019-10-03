ae66c8869af525056ad00834c5653f35cf1fac60
williamcll <c_william88@hotmail.com>
2019-10-03 17:08:12 +0800

    listview

---
+++ ListView/app/src/main/java/com/chuhai/listview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.chuhai.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    privateArrayList getListData() {}
+}
